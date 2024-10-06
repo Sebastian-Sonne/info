59072296a8d243754fcb0841ee3fabb1e80d6790
sebastian-sonne <sebastian@familie-sonne.com>
2024-10-06 16:21:18 +0200

    tailwind setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js}'],
+  content: ['./src/**/*.tsx', './index.html'],
+  darkMode: "class",
   theme: {
     extend: {},
   },
